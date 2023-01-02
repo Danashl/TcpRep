f7ba0cd9ec03f2c4fefa54c3ffa2716dcdd4e26d
RoniSedakah <ronisedakah@gmail.com>
2023-01-02 11:09:52 +0200

    changes in server.cpp

--- server.cpp
+++ server.cpp
@@ -101,66 +101,68 @@ int main (int argc, char *argv[]) {
     const int server_port = atoi(argv[2]);
     checkingArgv(server_port, file);
     int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if(sock < 0) {
-        cout << "error creating socket" <<endl;
+    if (sock < 0) {
+        cout << "error creating socket" << endl;
     }
     struct sockaddr_in sin;
     memset(&sin, 0, sizeof(sin));
     sin.sin_family = AF_INET;
     sin.sin_addr.s_addr = INADDR_ANY;
     sin.sin_port = htons(server_port);
-    if(bind(sock, (struct sockaddr*) &sin, sizeof(sin)) < 0) {
-        cout<<"error binding socket"<<endl;
+    if (bind(sock, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
+        cout << "error binding socket" << endl;
     }
-    if(listen(sock, 5) < 0) {
-        cout<<"error listening to a socket"<<endl;
+    if (listen(sock, 5) < 0) {
+        cout << "error listening to a socket" << endl;
     }
     struct sockaddr client_sin;
     unsigned int addr_len = sizeof(client_sin);
-    int client_sock = accept(sock, (struct sockaddr*) &client_sin, &addr_len);
-    if(client_sock < 0) {
-        cout<<"error accepting client"<<endl;
-    }
-    while(true) {
-        char buffer[4096];
-        int expected_data_len = sizeof(buffer);
-        int read_bytes = recv(client_sock, buffer, expected_data_len, 0);
-        if (read_bytes == 0) {
-            cout << "no message from client" << endl;
+    while (true) {
+        int client_sock = accept(sock, (struct sockaddr *) &client_sin, &addr_len);
+        if (client_sock < 0) {
+            cout << "error accepting client" << endl;
         }
-        if (read_bytes < 0) {
-            cout << "error getting a message from client" << endl;
-        }
-        data = buffer;
-        res = checkingStr(data, distance, vector, neighbor,flag);
-        //in case we found invalid input
-        if(res == -1) {
-            message = "invalid";
+        while (true) {
+            char buffer[4096];
+            int expected_data_len = sizeof(buffer);
+            int read_bytes = recv(client_sock, buffer, expected_data_len, 0);
+            if (read_bytes == 0) {
+                cout << "no message from client" << endl;
+            }
+            if (read_bytes < 0) {
+                cout << "error getting a message from client" << endl;
+            }
+            data = buffer;
+            res = checkingStr(data, distance, vector, neighbor, flag);
+            //in case we found invalid input
+            if (res == -1) {
+                message = "invalid";
+                int length = message.length();
+                char invalid_message[length + 1];
+                strcpy(invalid_message, message.c_str());
+                int send_bytes = send(client_sock, invalid_message, length, 0);
+                if (send_bytes < 0) {
+                    cout << "error sending a message" << endl;
+                }
+                break;
+            }
+            //if the input is valid
+            Knn *knn = new Knn(neighbor, distance, vector);
+            knn->uploadFiles(file, flag);
+            //check if flag is -1 - then something went wrong in knn class
+            if (flag == -1) {
+                message = "invalid";
+            } else {
+                message = knn->getMessage();
+            }
             int length = message.length();
-            char invalid_message[length+1];
-            strcpy(invalid_message, message.c_str());
-            int send_bytes = send(client_sock, invalid_message, length, 0);
+            char message_to_send[length + 1];
+            strcpy(message_to_send, message.c_str());
+            int send_bytes = send(client_sock, message_to_send, length, 0);
             if (send_bytes < 0) {
                 cout << "error sending a message" << endl;
             }
-            break;
+            delete knn;
         }
-        //if the input is valid
-        Knn *knn = new Knn(neighbor, distance, vector);
-        knn->uploadFiles(file,flag);
-        //check if flag is -1 - then something went wrong in knn class
-        if(flag == -1) {
-            message = "invalid";
-        } else {
-            message = knn->getMessage();
-        }
-        int length = message.length();
-        char message_to_send[length + 1];
-        strcpy(message_to_send, message.c_str());
-        int send_bytes = send(client_sock, message_to_send, length, 0);
-        if (send_bytes < 0) {
-            cout << "error sending a message" << endl;
-        }
-        delete knn;
     }
 }
